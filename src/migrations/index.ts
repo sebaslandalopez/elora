d858236920724a3c564fa0c472e541dece2a1410
Sebastián Landaeta <sebastianlandaeta12@gmail.com>
2026-09-14 11:17:08 -0400

    [ADD] DB en la nube

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260913_190057 from './20260913_190057';
+
+export const migrations = [
+  {
+    up: migration_20260913_190057.up,
+    down: migration_20260913_190057.down,
+    name: '20260913_190057'
+  },
+];
