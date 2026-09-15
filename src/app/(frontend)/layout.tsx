c37d69ba99c7c370ed7d22729bde0c828da1873b
Sebastián Landaeta <sebastianlandaeta12@gmail.com>
2026-09-15 09:53:41 -0400

    [FIX] Se corrigió un detalle en el nombre que aparece en el tab

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
-  title: 'Elona',
+  title: 'Elora',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
